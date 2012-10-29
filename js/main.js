ffe27393cfcc9d6f2f826c5cb888c5429500f9f8
Roger Steve Ruiz <roger.ruiz@rokkan.com>
2012-10-29 01:37:10 -0400

    Removing legacy code.
    This patch removes the original ajax call which was used while
    working on the proof of concept. It's since been integrated into the
    getSome object.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,4 @@
 $(function(){
-	// $.ajax({
-	// 	url: "https://api.instagram.com/v1/tags/halloween/media/recent?client_id=6954e6f84aad462e998b5b8d550730db",
-	// 	// url: "https://api.instagram.com/v1/tags/halloween/media/recent?client_id=6954e6f84aad462e998b5b8d550730db&min_tag_id=1351307871884",
-	// 	dataType: 'jsonp',
-	// 	crossDomain: true,
-	// 	success: function(instagram){
-	// 		var _i, _len, img;
-	// 		_i = 0;
-	// 		_len = instagram.data.length;
-	// 		for (_i; _i < _len; _i++) {
-	// 			img = instagram.data[_i].images.standard_resolution.url;
-	// 			$('#impress').append('<div class="slide step" data-x="' + _i * 0 + '" data-rotate="' + _i * 15 + '"  data-y="' + _i * 0 +  '" data-z="' + 1250 * _i + '">' +
-	// 					'<img src="' + img + '" alt="">' +
-	// 			'</div>');
-	// 		}
-	// 		if (console.log) { console.log('_len', _len); }
-	// 		impress().init();
-	// 		var nextImage = window.setInterval(function(){
-	// 			impress().next();
-	// 		}, 3500);
-	// 	}
-	// });
 
 	function getRandomInt(min, max){
 		return Math.floor(Math.random() * (max - min + 1)) + min;
